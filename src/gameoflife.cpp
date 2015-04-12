0c9cd8330485cadb458470858914c17b256d79ed
Luis Carlos Garcis-Peraza Herrera <luis.herrera.14@ucl.ac.uk>
2015-04-12 16:31:20 +0100

    Source file for GameOfLife class added.

---
+++ src/gameoflife.cpp
@@ -0,0 +1,37 @@
+/**
+ * @brief Implementation of the class GameOfLife.
+ *
+ * @author Luis Carlos Garcia-Peraza Herrera.
+ * @date   8 Apr 2015.
+ */
+
+// My includes
+#include "gameoflife.h"
+
+/**
+ * TODO
+ */
+GameOfLife::GameOfLife() {
+
+}
+
+/**
+ * TODO
+ */
+void GameOfLife::readConfig(const std::string &path) {
+
+}
+
+/**
+ * TODO
+ */
+void GameOfLife::iterate() {
+
+}
+
+/**
+ * TODO
+ */
+void GameOfLife::writeOutputFile(const std::string &path) {
+
+}
