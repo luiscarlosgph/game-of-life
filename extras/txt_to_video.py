44e48d35efe9a09dc3bea20c1695007c94560ad8
Luis Carlos Garcis-Peraza Herrera <luis.herrera.14@ucl.ac.uk>
2015-04-17 16:40:38 +0100

    First version of the Python script that converts the output file of the program to a video.

--- extras/txt_to_video.py
+++ extras/txt_to_video.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python
+# 
+# Script to convert the output file produced by the program GameOfLife into a video.
+# 
+# Dependencies:
+# =============
+#
+# 1) FFmpeg version 2.4.2
+# 
+# Author: Luis Carlos Garcia-Peraza Herrera (luis.herrera.14@ucl.ac.uk).
+# Date  : 15 Apr 2015.
 
+import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+import cv2
+import os
+import shutil
 from sys import argv
 
 def createMatrixFromString(str):
@@ -31,22 +45,69 @@ def createMatrixFromString(str):
 	
 	return mat
 
-# def createImageFromMatrix(mat):
-	# TODO	
-	# plt.plot([1,2,3,4], [1,4,9,16], 'ro')
-	# plt.axis([0, 6, 0, 20])
-	# plt.show()
+def createImageFromMatrix(mat):
+	# Create colormap of fixed colors (two tones of blue, one darker (representing alive cells) than the other one)
+	cmap = mpl.colors.ListedColormap([[0.84, 0.92, 1], [0.11, 0.58, 1]])
+	bounds = [-0.1, 0.1, 0.9, 1.1]
+	norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
+	
+	# Hide axes text
+	frame = plt.gca()
+	frame.axes.get_xaxis().set_visible(False)
+	frame.axes.get_yaxis().set_visible(False)
+
+	# Set the previous color map as the one to be used when plotting
+	img = plt.imshow(mat, interpolation = 'nearest', cmap = cmap, norm = norm)
+	
+	# Convert image to OpenCV format
+	tmpFile = '/tmp/.temp_conv.png'
+	plt.savefig(tmpFile, format = 'png')
+	cvImg = cv2.imread(tmpFile, cv2.IMREAD_UNCHANGED)
+	os.remove(tmpFile)
+
+	return cvImg
+
+def createVideoFromImages(images, outputFilePath):
+	#height, width, layers = images[0].shape
+	# video = cv2.VideoWriter(outputFilePath, -1, 1, (width, height))
+	#fourcc = cv2.cv.CV_FOURCC(*'XVID')
+	#video = cv2.VideoWriter(outputFilePath, fourcc, 20.0, (width, height))
+
+	#for image in images:
+	#	for j in xrange(25):
+	#		video.write(image)
+	# cv2.destroyAllWindows()
+
+	#video.release()
+
+	# Create temporary dir for the pictures
+	temporaryFolder = '/tmp/.temporaryVideoFolder'
+	if not os.path.exists(temporaryFolder):
+	    os.makedirs(temporaryFolder)
+	else:
+		raise Exception('The temporary folder used to generate the video already exists. Delete it first!')
+	
+	# Save all the pictures in order
+	for i in xrange(len(images)):
+		cv2.imwrite(temporaryFolder + '/image' + '{0:05d}'.format(i) + '.png', images[i])
+	
+	# Convert images to video
+	speed = 5
+	retvalue = os.system('ffmpeg -f image2 -r ' + str(speed) + ' -i ' + temporaryFolder + '/image%05d.png -vcodec mpeg4 -y ' + outputFilePath)
+	
+	# Delete temporary folder and its contents
+	shutil.rmtree(temporaryFolder)
 
 def getHistoryFileAsString(fname):
 	with open(fname) as f:
-	    content = f.readlines()
+		content = f.readlines()
 	return content
 
 def main():
-	script, filename = argv
+	script, inputFile, outputFile = argv
 
 	# Read history file and convert it to a list of lines
-	lines = getHistoryFileAsString(filename)
+	lines = getHistoryFileAsString(inputFile)
 	
 	# Convert lines to a list of images
 	listOfImages = []
@@ -55,16 +116,14 @@ def main():
 		if line in ['\n', '\r\n']:
 			boardStr = boardStr[:-1]
 			mat = createMatrixFromString(boardStr)
+			img = createImageFromMatrix(mat)	
+			listOfImages.append(img)
 			boardStr = ''
-			print mat
-			# img = generateBoardImageFromMatrix(mat)	
-			# listOfImages.append(img)
 		else:
 			boardStr += line
 	
 	# Convert the list of images into a video
-	# for image in listOfImages:
-	# TODO	
+	createVideoFromImages(listOfImages, outputFile)	
 	
 if __name__ == "__main__":
 	main()
