d4f1f7c925e0c07f6ef369ca1d59313a58a82c91
Luis Carlos Garcis-Peraza Herrera <luis.herrera.14@ucl.ac.uk>
2015-04-26 03:25:31 +0100

    Updated build.sh to change the cmake command depending on the OS.

--- build.sh
+++ build.sh
@@ -3,7 +3,13 @@
 rm -rf build
 mkdir build
 cd build
-cmake .. -DCMAKE_CXX_COMPILER=/usr/local/bin/g++ -DCMAKE_CC_COMPILER=/usr/local/bin/gcc
+
+if [ "$(uname)" == "Darwin" ]; then
+	cmake .. -DCMAKE_CXX_COMPILER=/usr/local/bin/g++ -DCMAKE_CC_COMPILER=/usr/local/bin/gcc
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+	cmake ..
+fi
+
 make
 cp src/GameOfLife ../bin/
 cp test/GameOfLifeTest ../bin/
